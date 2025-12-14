835905dc9e075c71cb34a628c01812a1a9ff961f
marialogashova <logasovamaria09@gmail.com>
2025-12-14 20:37:33 +0300

    //

--- biblioteka123/biblioteka.L.h
+++ biblioteka123/biblioteka.L.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+namespace User
+{
+	class People
+	{
+	private:
+		int userID;
+	public:
+		People(int ID);
+
+		void ShowBooksCstslog();
+		void ShowMyBooks();
+	};
+}
