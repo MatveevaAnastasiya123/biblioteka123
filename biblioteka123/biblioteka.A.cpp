72668a1d27d2f61a8dffe70a8d6b90e9538661a4
aliyaayurova <abolotova328@gmail.com>
2025-12-15 23:24:17 +0300

    ....

--- biblioteka123/biblioteka.A.cpp
+++ biblioteka123/biblioteka.A.cpp
@@ -1,1 +1,78 @@
 #include <iostream>
+"biblioteka.A.h"
+using namespace std;
+
+
+Literature::Literature() {
+	title = "Без названия";
+	author = "Неизвестен";
+	year = 2000;
+}
+
+Literature::Literature(string t, string a, int y) {
+	title = t;
+	author = a;
+	year = y;
+}
+
+void Literature::display() {
+	cout << "Название: " << title << endl;
+	cout << "Автор: " << author << endl;
+	cout << "Год: " << year << endl;
+}
+
+void Literature::setInfo(string t, string a, int y) {
+	title = t;
+	author = a;
+	year = y;
+}
+string Literature::getTitle() {
+	return title;
+}
+string Literature::getAuthor() {
+	return author;
+}
+int Literature::getYear() {
+	return year;
+}
+
+
+Magazine::Magazine() : Literature() {
+	issue = 1;
+	category = "Общий";
+}
+
+Magazine::Magazine(string t, string a, int y, int i, string c) : Literature(t, a, y) {
+	issue = i;
+	category = c;
+}
+
+void Magazine::displayMagazine() {
+	cout << "\n===Журнал===" << endl;
+	display();
+	cout << "Номер выпуска: " << issue << endl;
+	cout << "Категория: " << category << endl;
+	cout << "=======================" << endl;
+}
+
+void Magazine::nextIssue() {
+	issue++;
+	year++;
+	cout << "Выпущен следующий номер: " << issue << " (" << year << " год)" << endl;
+}
+
+int Magazine::getIssue() {
+	return issue;
+}
+
+string Magazine::getCategory() {
+	return category;
+}
+
+void Magazine::setIssue(int i) {
+	issue = i;
+}
+
+void Magazine::setCategory(string c) {
+	category = c;
+}
--- biblioteka123/biblioteka.A.h
+++ biblioteka123/biblioteka.A.h
@@ -1,1 +1,45 @@
 #pragma once
+
+<iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+class Literature {
+private:
+	string title;
+	string author;
+	int year;
+
+public:
+	Literature();
+	Literature(string t, string a, int y);
+
+
+	void display();
+	void setInfo(string t, string a, int y);
+
+	string getTitle();
+	string getAuthor();
+	int getYear();
+};
+
+class Magazine : public Literature {
+private:
+	int issue;
+	string category;
+
+public:
+	Magazine();
+	Magazine(string t, string a, int y, int i, string c);
+
+	void displayMagazine();
+	void setMagazineInfo(string t, string a, int y, int i, string c);
+	void nextIssue();
+
+	int getIssue();
+	string getCategory();
+
+	void setIssue(int i);
+	void setCategory(string c);
+};
