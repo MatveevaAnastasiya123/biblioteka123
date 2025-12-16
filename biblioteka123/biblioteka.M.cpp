a9e6ae20a591a3e47078beef2b267649493e3fa9
MatveevaAnastasiya123 <nastammm4@gmail.com>
2025-12-16 15:23:39 +0300

    Update biblioteka.M.cpp

--- biblioteka123/biblioteka.M.cpp
+++ biblioteka123/biblioteka.M.cpp
@@ -3,13 +3,13 @@
 void Librarian::addUser() {
     cout << "=====ДОБАВЛЕНИЕ ПОЛЬЗОВАТЕЛЯ=====" << endl;
     string fio, birthday, tel, adress;
-    cout << "Ââåäèòå ÔÈÎ:";
+    cout << "Введите ФИО:";
     getline(cin, fio);
-    cout << "Ââåäèòå äàòó ðîæäåíèÿ:";
+    cout << "Введите дату рождения:";
     getline(cin, birthday);
-    cout << "Ââåäèòå òåëåôîí:";
+    cout << "Введите телефон:";
     getline(cin, tel);
-    cout << "Ââåäèòå àäðåñ:";
+    cout << "Введите адрес:";
     getline(cin, adress);
 
     ofstream file("users.txt", ios::app);
@@ -20,23 +20,23 @@ void Librarian::addUser() {
         file << adress << endl;
         file << "-------------" << endl;
         file.close();
-        cout << "Íîâûé ïîëüçîâàòåëü " << fio << " äîáàâëåí." << endl;
+        cout << "Новый пользователь " << fio << " добавлен." << endl;
     }
     else {
-        cout << "Ïðîèçîøëà îøèáêà ïðè ðåãèñòðàöèè ïîëüçîâàòåëÿ.";
+        cout << "Ошибка записи пользователя в файл.";
         return;
     }
 }
 
 void Librarian::delUser() {
-    cout << "=====ÓÄÀËÅÍÈÅ ÏÎËÜÇÎÂÀÒÅËß=====" << endl;
+    cout << "=====УДАЛЕНИЕ ПОЛЬЗОВАТЕЛЯ=====" << endl;
     string FIO_del;
-    cout << "Ââåäèòå ÔÈÎ ïîëüçîâàòåëÿ, êîòîðîãî õîòèòå óäàëèòü: ";
+    cout << "Введите ФИО пользователя, которого хотите удалить: ";
     getline(cin, FIO_del);
 
     ifstream file("users.txt");
     if (!file) {
-        cout << "Îøèáêà îòêðûòèÿ ôàéëà." << endl;
+        cout << "Ошибка открытия файла с пользователями." << endl;
         return;
     }
 
@@ -66,26 +66,26 @@ void Librarian::delUser() {
     if (found) {
         remove("users.txt");
         rename("temp.txt", "users.txt");
-        cout << "Ïîëüçîâàòåëü óäàëåí. " << endl;
+        cout << "Пользователь удален. " << endl;
     }
     else {
         remove("temp.txt");
-        cout << "Íå íàéäåí!" << endl;
+        cout << "Пользователь не найден." << endl;
     }
 }
 
 void Librarian::addBook() {
-    cout << "=====ÄÎÁÀÂËÅÍÈÅ ÊÍÈÃÈ=====" << endl;
+    cout << "=====ДОБАВЛЕНИЕ КНИГИ=====" << endl;
     string author, title, edition, annatation, year;
-    cout << "Ââåäèòå íàçâàíèå êíèãè:";
+    cout << "Введите название книги:";
     getline(cin, title);
-    cout << "Ââåäèòå ÔÈÎ àâòîðà: ";
+    cout << "Введите ФИО автора: ";
     getline(cin, author);
-    cout << "Ââåäèòå ãîä èçäàíèÿ: ";
+    cout << "Введите год издания: ";
     getline(cin, year);
-    cout << "Ââåäèò íàçâàíèå èçäàíèÿ: ";
+    cout << "Введите название издания: ";
     getline(cin, edition);
-    cout << "Ââåäèòå àíîòàöèþ ê êíèãå: ";
+    cout << "Введите анотацию к книге: ";
     getline(cin, annatation);
 
     ofstream file("books.txt", ios::app);
@@ -97,18 +97,18 @@ void Librarian::addBook() {
         file << annatation << endl;
         file << "---------------------" << endl;
         file.close();
-        cout << "Êíèãà " << title << " äîáàâëåíà â áèáëèîòåêó." << endl;
+        cout << "Книга " << title << " добавлена в библиотеку." << endl;
     }
 }
 
 void Librarian::delBook() {
-    cout << "=====ÓÄÀËÅÍÈÅ ÊÍÈÃÈ=====" << endl;
+    cout << "=====УДАЛЕНИЕ КНИГИ=====" << endl;
     string del_title;
-    cout << "Ââåäèòå íàçâàíèå êíèãè, êîòîðóþ õîòèòå óäàëèòü: ";
+    cout << "Введите название книги, которую хотите удалить: ";
     getline(cin, del_title);
     ifstream file("books.txt");
     if (!file) {
-        cout << "Îøèáêà çàïèñè êíèãè â ôàéë.";
+        cout << "Ошибка открытия файла с книгами.";
         return;
     }
     ofstream temp("temp.txt");
@@ -133,11 +133,11 @@ void Librarian::delBook() {
     if (found) {
         remove("books.txt");
         rename("temp.txt", "books.txt");
-        cout << "Êíèãà " << del_title << " óäàëåíà." << endl;
+        cout << "Книга " << del_title << " удалена." << endl;
     }
     else {
         remove("temp.txt");
-        cout << "Êíèãà " << del_title << " íå íàéäåíà." << endl;
+        cout << "Книга " << del_title << " не удалена." << endl;
     }
 }
 
