8c96b450694c52fada8ac73e61f15d8441aae9a1
MatveevaAnastasiya123 <nastammm4@gmail.com>
2025-12-17 17:11:08 +0300

    Update biblioteka.Lat.h

--- biblioteka123/biblioteka.Lat.h
+++ biblioteka123/biblioteka.Lat.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "biblioteka.B.h"
+class ELiter : public Literature {
+public:
+    string format;
+    string size;
+    string description;
+    
+    void showAvailable();
+};
