8b1b2b9285dbff50a914db4fe3128075b4ad6e89
MatveevaAnastasiya123 <nastammm4@gmail.com>
2025-12-18 06:52:44 +0300

    Update biblioteka.B.h

--- biblioteka123/biblioteka.B.h
+++ biblioteka123/biblioteka.B.h
@@ -1,1 +1,14 @@
 #pragma once
+using namespace std;
+
+class Literature {
+public:
+string title;
+string author;
+string year;
+};
+
+class Books : public Literature {
+public:
+void showAvailable();
+};
